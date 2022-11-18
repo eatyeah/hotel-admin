208cad330368961b16b474b1b55b2fa66bae9d98
sweeterjava <sweeterjava@163.com>
2022-11-19 02:42:21 +0800

    解决跨域问题

--- src/main/java/com/daigg/hoteladmin/config/Knife4jConfiguration.java
+++ src/main/java/com/daigg/hoteladmin/config/Knife4jConfiguration.java
@@ -28,7 +28,7 @@ public class Knife4jConfiguration {
     /**
      * 【重要】指定Controller包路径
      */
-    private String basePackage = "cn.tedu.csmall.product.controller";
+    private String basePackage = "com.daigg.hoteladmin.controller";
     /**
      * 分组名称
      */
